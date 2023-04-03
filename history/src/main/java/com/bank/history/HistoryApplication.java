6f0b1f518dff4d2a52ac6fc84cbe2b8df318b693
inntors <107668595+inntors@users.noreply.github.com>
2023-04-03 19:54:50 +0300

    init commit

---
+++ history/src/main/java/com/bank/history/HistoryApplication.java
@@ -0,0 +1,11 @@
+package com.bank.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HistoryApplication.class, args);
+    }
+}
---
+++ public-info/src/main/java/com/bank/publicinfo/PublicInfoApplication.java
@@ -0,0 +1,11 @@
+package com.bank.publicinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicInfoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PublicInfoApplication.class, args);
+    }
+}
