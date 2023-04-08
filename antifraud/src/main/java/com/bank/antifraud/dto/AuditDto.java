9ddd3fd327f5a622f9272b5b7cb3b04f3af00b62
Makariy <makariyp@icloud.com>
2023-04-08 13:33:25 +0300

    Добавил JavaDoc комментарии для классов, которые работают с сущностью Audit

--- antifraud/src/main/java/com/bank/antifraud/dto/AuditDto.java
+++ antifraud/src/main/java/com/bank/antifraud/dto/AuditDto.java
@@ -3,6 +3,10 @@ package com.bank.antifraud.dto;
 import javax.persistence.Column;
 import java.sql.Timestamp;
 
+/**
+ * Класс, описывающий Data Transfer Objects Audit
+ * @author Makariy Petrov
+ */
 public record AuditDto
         (Long id,
          String entityType,
--- antifraud/src/main/java/com/bank/antifraud/repository/AuditRepository.java
+++ antifraud/src/main/java/com/bank/antifraud/repository/AuditRepository.java
@@ -4,6 +4,10 @@ import com.bank.antifraud.entity.AuditEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Слой доступа к таблице Audit в БД
+ * @author Makariy Petrov
+ */
 @Repository
 public interface AuditRepository extends JpaRepository<AuditEntity, Long> {
 }
